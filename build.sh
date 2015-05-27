f75cd4e4f4220e94a31fa438a0a61c2d8e28f023
Jasper Blues <jasper@appsquick.ly>
2015-05-27 16:41:29 +0800

    Remove ./ in --source-dir argument for coverage report. (It presents directory names in the HTML report better).

--- build.sh
+++ build.sh
@@ -5,4 +5,4 @@ rm -rf ~/Library/Developer/Xcode/DerivedData/TyphoonRestClient-*
 xcodebuild test -workspace TyphoonRestClient.xcworkspace/ -scheme 'Tests' -configuration Debug \
 -destination 'platform=iOS Simulator,name=iPhone 5s,OS=8.3' | xcpretty -c --report junit
 
-groovy http://frankencover.it/with -source-dir ./TyphoonRestClient -required-coverage 85
+groovy http://frankencover.it/with -source-dir TyphoonRestClient -required-coverage 85
