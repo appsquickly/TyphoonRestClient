eeebfc37e9c4526974817439039d2d7e5ced2feb
Aleksey Garbarev <alex.garbarev@gmail.com>
2015-06-30 16:06:40 +0600

    Build Schema changed

--- build.sh
+++ build.sh
@@ -7,7 +7,7 @@ set -o pipefail
 rm -rf ~/Library/Developer/Xcode/DerivedData/TyphoonRestClient-*
 rm -fr ./build
 
-xcodebuild test -workspace TyphoonRestClient.xcworkspace/ -scheme 'Tests' -configuration Debug \
+xcodebuild test -workspace TyphoonRestClient.xcworkspace/ -scheme 'TyphoonRestClient' \
 -destination 'platform=iOS Simulator,name=iPhone 5s,OS=8.3' | xcpretty -c --report junit
 
 groovy http://frankencover.it/with -source-dir TyphoonRestClient -required-coverage 85
