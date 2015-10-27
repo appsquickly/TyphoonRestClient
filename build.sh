708997ce3407e692e447c942aa1d662d76c7c89f
Aleksey Garbarev <alex.garbarev@gmail.com>
2015-10-27 20:01:34 +0600

    build script updated

--- build.sh
+++ build.sh
@@ -1,7 +1,5 @@
 #!/bin/sh
 
-pod update
-
 #Fail immediately if a task fails
 set -e
 set -o pipefail
@@ -9,7 +7,7 @@ set -o pipefail
 rm -rf ~/Library/Developer/Xcode/DerivedData/TyphoonRestClient-*
 rm -fr ./build
 
-xcodebuild test -workspace TyphoonRestClient.xcworkspace/ -scheme 'TyphoonRestClient' \
--destination 'platform=iOS Simulator,name=iPhone 5s,OS=8.3' | xcpretty -c --report junit
+xcodebuild test -project TyphoonRestClient.xcodeproj/ -scheme 'TyphoonRestClient' \
+-destination 'platform=iOS Simulator,name=iPhone 6,OS=latest' | xcpretty -c --report junit
 
-groovy http://frankencover.it/with -source-dir TyphoonRestClient -required-coverage 85
+# groovy http://frankencover.it/with -d -source-dir TyphoonRestClient -required-coverage 85
